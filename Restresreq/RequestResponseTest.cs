9a308f5d71eb8d13014d3c0a5dff65eb08cafbb6
adonthumalla <124860473+adonthumalla@users.noreply.github.com>
2023-02-22 20:23:27 +1100

    Added Rest Req Res Project

---
+++ Restresreq/RequestResponseTest.cs
@@ -0,0 +1,111 @@
+﻿using APIAutomationSetup.Models;
+using APIAutomationSetup.Models_DTO;
+using Newtonsoft.Json;
+using Restresreq.Models;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APIAutomationSetup
+{
+    public class RequestResponseTest
+    {
+        private UserHelper userHelper;
+
+        public RequestResponseTest()
+        {
+            userHelper = new UserHelper();
+        }
+
+        public UserHelper UserHelper { get; private set; }
+
+        public Users getUsers()
+        {
+            var client = userHelper.SetUrl("api/users?page=2");
+            var request = userHelper.CreateGetRequest();
+            request.RequestFormat = DataFormat.Json;
+            var response = userHelper.GetResponse(client, request);
+            var users = userHelper.GetContent<Users>(response);
+            return users;
+        }
+
+        public CreateUserRes CreateNewUser(string payload)
+        {
+            var client = userHelper.SetUrl("api/users");
+            var request = userHelper.CreatePostRequest(payload);
+            var response = userHelper.GetResponse(client, request);
+            var createuser = userHelper.GetContent<CreateUserRes>(response);
+            return createuser;
+        }
+
+        public Users getAllUsers()
+        {
+            var client = userHelper.SetUrl("api/users?page=2");
+            var request = userHelper.CreateGetRequest();
+            request.RequestFormat = DataFormat.Json;
+            var response = userHelper.GetResponse(client, request);
+            var users = userHelper.GetContent<Users>(response);
+            return users;
+        }
+
+        public UpdateUserRes CreatePutRequest(string payload)
+        {
+            var client = userHelper.SetUrl("api/users/2");
+            var request = userHelper.CreatePutRequest(payload);
+            var response = userHelper.GetResponse(client, request);
+            var updateuser = userHelper.GetContent<UpdateUserRes>(response);
+            return updateuser;
+
+        }
+
+        public int CreateDeleteRequest()
+        {
+            var client = userHelper.SetUrl("api/users/2");
+            var request = userHelper.CreateDeleteRequest();
+            var response = userHelper.GetResponse(client, request);
+            var statusCode = userHelper.GetStatusCode(response);
+            return statusCode;
+
+        }
+
+        public RegisterUserRes CreateRegisterRequest(string payload)
+        {
+            var client = userHelper.SetUrl("api/register");
+            var request = userHelper.CreateRegisterORLoginRequest(payload);
+            var response = userHelper.GetResponse(client, request);
+            var registerUserRes = userHelper.GetContent<RegisterUserRes>(response);
+            return registerUserRes;
+        }
+
+        public int CreateRegisterUnsuccessfulRequest(string payload)
+        {
+            var client = userHelper.SetUrl("api/register");
+            var request = userHelper.CreateRegisterORLoginRequest(payload);
+            var response = userHelper.GetResponse(client, request);
+            var statusCode = userHelper.GetStatusCode(response);
+            return statusCode;
+        }
+
+        public LoginUserRes CreateUserLoginReq(string payload)
+        {
+            var client = userHelper.SetUrl("api/login");
+            var request = userHelper.CreateRegisterORLoginRequest(payload);
+            var response = userHelper.GetResponse(client, request);
+            var loginUserRes = userHelper.GetContent<LoginUserRes>(response);
+            return loginUserRes;
+        }
+
+        public int getSingleResourceReq(string uri)
+        {
+            var client = userHelper.SetUrl(uri);
+            var request = userHelper.CreateGetRequest();
+            request.RequestFormat = DataFormat.Json;
+            var response = userHelper.GetResponse(client, request);
+            var statusCode = userHelper.GetStatusCode(response);
+            return statusCode;
+        }
+    }
+}
---
+++ Restresreq/UserHelper.cs
@@ -0,0 +1,91 @@
+﻿using Azure;
+using Newtonsoft.Json;
+using Restresreq.Models;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace APIAutomationSetup
+{
+
+    // Create a client for the API
+    public class UserHelper
+    {
+        private RestClient client;
+        private RestRequest request;
+        private const string baseUrl = "https://reqres.in/";
+
+        public RestClient SetUrl(string endpoint) { 
+            var url = Path.Combine(baseUrl, endpoint);
+            client = new RestClient(url);
+            return client;
+        }
+        /// Add any query parameters or headers needed
+        public RestRequest CreateGetRequest() { 
+            request= new RestRequest();
+            request.AddHeader("Accept", "application/json");
+            return request;
+        }
+        /// Add any query parameters or headers needed
+        public RestRequest CreatePostRequest(string payload)
+        {
+            request= new RestRequest("", Method.Post);
+            request.AddHeader("Accept", "application/json");
+            request.AddParameter("application/json",payload, ParameterType.RequestBody);
+            return request;
+        }
+
+        /// Add any query parameters or headers needed existing user data from the API
+        public RestResponse GetResponse(RestClient restClient,RestRequest restRequest)
+        {
+            return restClient.Execute(restRequest);
+        }
+
+        public T GetContent<T>(RestResponse restResponse) { 
+            var content = restResponse.Content;
+            return JsonConvert.DeserializeObject<T>(content);
+        }
+
+        /// Add any query parameters or headers to update the user data from the API
+        public RestRequest CreatePutRequest(string payload)
+        {
+            request = new RestRequest(" ", Method.Put);
+            request.AddHeader("Accept", "application/json");
+            request.AddParameter("application/json", payload, ParameterType.RequestBody);
+            return request;
+
+        }
+
+        /// Add any query parameters or headers to delete the user data from the API
+        public RestRequest CreateDeleteRequest()
+        {
+            request = new RestRequest("", Method.Delete);
+            request.AddHeader("Accept", "application/json");
+            return request;
+        }
+
+        public int GetStatusCode(RestResponse restResponse) {
+            HttpStatusCode statusCode = restResponse.StatusCode;
+            int httpStatusCode = (int)statusCode;
+            return httpStatusCode;
+
+        }
+
+        /// Add any query parameters or headers to register the user data from the API
+        public RestRequest CreateRegisterORLoginRequest(string payload)
+        {
+            request = new RestRequest(" ", Method.Post);
+            request.AddHeader("Accept", "application/json");
+            request.AddParameter("application/json", payload, ParameterType.RequestBody);
+            return request;
+        }
+    }
+
+}
